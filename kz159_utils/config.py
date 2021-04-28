2dbe9a27f41b3129de14af49c822520c9bf24630
Georgy <loh@kz159.ru>
2021-04-28 21:47:50 +0300

    upd

--- kz159_utils/config.py
+++ kz159_utils/config.py
@@ -3,20 +3,10 @@ from copy import deepcopy
 from os import getenv, chdir
 from os.path import abspath, dirname
 
-try:
-    from dotenv import load_dotenv
-
-    abspath = abspath(__file__)
-    dname = dirname(dirname(abspath))
-    chdir(dname)
-
-    load_dotenv('.env')
-except ImportError:
-    pass
-
 
 class Config:
-    def __init__(self, service_name: str = __name__):
+    def __init__(self, abs_path, service_name: str = __name__, ):
+        self.try_to_load_dot_env(abs_path)
         self.SERVICE_NAME = service_name
         self.POSTGRES_HOST = getenv('POSTGRES_HOST', 'localhost')
         self.POSTGRES_PASSWORD = getenv('POSTGRES_PASSWORD', 'postgres')
@@ -24,6 +14,19 @@ class Config:
         self.POSTGRES_USER = getenv('POSTGRES_USER', 'postgres')
         self.POSTGRES_DB_NAME = getenv('POSTGRES_DB_NAME', 'postgres')
         self.LOG_LEVEL = getenv('LOG_LEVEL', 'INFO')
+
+    @staticmethod
+    def try_to_load_dot_env(abs_path):
+        try:
+            # noinspection PyUnresolvedReferences
+            from dotenv import load_dotenv
+
+            abs_path = abspath(abs_path)
+            dname = dirname(dirname(abs_path))
+            chdir(dname)
+            return load_dotenv('.env')
+        except ImportError:
+            pass
         # add here rmq/etc
 
     @property
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='kz159_utils',
-    version='0.0.3',
+    version='0.0.4',
     packages=['kz159_utils'],
     url='https://github.com/kz159/simple_config',
     license='MIT',
